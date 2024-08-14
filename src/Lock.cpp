4d6e737a1778496f8316a36d7e970cf8c894d209
Tamim  Rashid <rashidytamimu@gmail.com>
2024-08-14 16:15:43 +0300

    update

---
+++ src/Lock.cpp
@@ -0,0 +1,13 @@
+#include"Lock.h";
+#include<Arduino.h>
+
+
+Lock::Lock(int trigger, unsigned long backresponse): trigger(backresponse){
+
+    pinMode(trigger, OUTPUT);
+
+}
+void Lock:: Open(){
+    digitalWrite(trigger, HIGH);
+    delay(backresponse);
+}
