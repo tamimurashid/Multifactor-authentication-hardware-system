c8dc7db15d5c51d5376e23922a6f7bd5a17adec4
Tamim  Rashid <rashidytamimu@gmail.com>
2024-08-21 15:44:26 +0300

    update

--- rfidimp.cpp
+++ rfidimp.cpp
@@ -2,13 +2,14 @@
 #include "Rfid.h"
 
 // Define the RFID pins based on your connections
-#define SS_PIN 4  // SDA/SS connected to GPIO4 (SC)
-#define RST_PIN 2 // You can connect the RST pin to a free GPIO, e.g., GPIO2 (D4)
+#define SS_PIN 4   // SDA/SS connected to GPIO4 (SC)
+#define RST_PIN 2  // RST pin connected to GPIO2 (D4)
 #define SCK_PIN 14 // SCK connected to GPIO14 (D5)
-#define MOSI_PIN 13 // MOSI connected to GPIO13 (D7)
+#define MOSI_PIN 13 // MOSI connected to GPIO13 (D4)
 #define MISO_PIN 12 // MISO connected to GPIO12 (D6)
 
-Rfid rfid(SS_PIN, RST_PIN);  // Create an instance of the Rfid class
+// Create an instance of the Rfid class with your defined pins
+Rfid rfid(SS_PIN, RST_PIN);
 
 void setup() {
     Serial.begin(115200);
@@ -30,3 +31,4 @@ void loop() {
 
     delay(1000);  // Delay for a second before checking again
 }
+
